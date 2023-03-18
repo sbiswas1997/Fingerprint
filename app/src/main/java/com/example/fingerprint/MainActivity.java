91edf6149e14c991232de5ac72e1c938cceab910
sbiswas1997 <biswassoumyabrata4@gmail.com>
2023-03-18 23:50:13 +0530

    onclicklistener

--- app/src/main/java/com/example/fingerprint/MainActivity.java
+++ app/src/main/java/com/example/fingerprint/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.fingerprint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv=(TextView) findViewById(R.id.id2);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("This is how on click listener work!");
+            }
+        });
     }
 }
