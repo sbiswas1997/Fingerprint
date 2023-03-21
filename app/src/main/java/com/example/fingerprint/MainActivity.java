dc93709a2e6cf1b7249beda29ea4865577cb9c21
sbiswas1997 <biswassoumyabrata4@gmail.com>
2023-03-21 23:26:02 +0530

    Added Fingerprint

--- app/src/main/java/com/example/fingerprint/MainActivity.java
+++ app/src/main/java/com/example/fingerprint/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.fingerprint;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.skyfishjy.library.RippleBackground;
+
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
+    RippleBackground rippleBackground;
+    ToggleButton toggleButton;
+
+    BiometricPrompt biometricPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
+    RelativeLayout mainlayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +34,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv=(TextView) findViewById(R.id.id2);
+        toggleButton = (ToggleButton) findViewById(R.id.tbtn);
+        rippleBackground = findViewById(R.id.rippleBg);
+        mainlayout = findViewById(R.id.mainActivity);
 
-        tv.setOnClickListener(new View.OnClickListener() {
+
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()){
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(getApplicationContext(),"Device doesn't have fingerprint sensor", Toast.LENGTH_SHORT);
+                break;
+
+                case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                    Toast.makeText(getApplicationContext(),"Biometric Not working", Toast.LENGTH_SHORT);
+                    break;
+
+                    case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                        Toast.makeText(getApplicationContext(),"No fingerprint assigned", Toast.LENGTH_SHORT);
+                        break;
+        }
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+        biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
             @Override
-            public void onClick(View view) {
-                tv.setText("This is how on click listener work!");
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),"Successfully logged in.", Toast.LENGTH_SHORT);
+                mainlayout.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
             }
         });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("LOGIN")
+                        .setDescription("Use fingerprint to login").setDeviceCredentialAllowed(true).build();
+        biometricPrompt.authenticate(promptInfo);
+
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (toggleButton.isChecked()){
+                    rippleBackground.startRippleAnimation();
+                }
+                else {
+                    rippleBackground.stopRippleAnimation();
+                }
+            }
+        });
+
+    }
+    public void textView(View view){
+        tv.setText("This is how onClick works!");
     }
 }
